5124714421977d6cf3171853a8eeb994a90eb470
KellenYang <15000642514@163.com>
2015-08-28 17:51:05 +0800

    AD

--- MyOne/MyOne/AboutViewController.m
+++ MyOne/MyOne/AboutViewController.m
@@ -10,17 +10,44 @@
 
 @interface AboutViewController ()
 
+@property (strong, nonatomic) UIWebView *webView;
+
 @end
 
 @implementation AboutViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    
-    
+    self.view.backgroundColor = [UIColor whiteColor];
+    self.view.nightBackgroundColor = NightBGViewColor;
+    [self setTitleView];
+    [self setUpViews];
+    [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://m.wufazhuce.com/about?from=ONEApp"]]];
 }
 
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    self.webView.frame = self.view.bounds;
+}
 
+- (void)setTitleView {
+    UILabel *titleLabel = [[UILabel alloc] init];
+    titleLabel.text = @"关于";
+    titleLabel.textColor = TitleTextColor;
+    titleLabel.nightTextColor = TitleTextColor;
+    titleLabel.font = [UIFont fontWithName:@"HelveticaNeue-Medium" size:18];
+    [titleLabel sizeToFit];
+    self.navigationItem.titleView = titleLabel;
+}
+
+- (void)setUpViews {
+    self.webView = [[UIWebView alloc]initWithFrame:self.view.bounds];
+    self.webView.scalesPageToFit = NO;
+    self.webView.multipleTouchEnabled = NO;
+    self.webView.exclusiveTouch = NO;
+    self.webView.backgroundColor = [UIColor colorWithRed:249 / 255.0 green:249 / 255.0 blue:249 / 255.0 alpha:1];
+    self.webView.scrollView.scrollsToTop = YES;
+    [self.view addSubview:self.webView];
+}
 
 @end
