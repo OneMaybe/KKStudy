1363aeec3ad77ca8359dbc6ae9d170a1f3c2b94b
KellenYang <15000642514@163.com>
2015-08-28 14:52:31 +0800

    项目初建/MyOne

---
+++ MyOne/MyOne/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyOne
+//
+//  Created by bcmac3 on 15/8/28.
+//  Copyright (c) 2015年 KellenYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyOne/MyOne/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyOne
+//
+//  Created by bcmac3 on 15/8/28.
+//  Copyright (c) 2015年 KellenYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
